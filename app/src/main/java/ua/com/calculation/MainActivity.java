d57107704b6e5b7c38e275d209c7cd9e6b96614a
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-10-19 13:50:32 +0300

    add calculate

--- app/src/main/java/ua/com/calculation/MainActivity.java
+++ app/src/main/java/ua/com/calculation/MainActivity.java
@@ -3,12 +3,45 @@ package ua.com.calculation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private EditText number_field_one, number_field_two;
+    private Button add_button;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.resultTextView);
+        number_field_one = findViewById(R.id.number_field_one);
+        number_field_two = findViewById(R.id.number_field_two);
+        add_button = findViewById(R.id.add_button);
+
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float num1 = Float.parseFloat(number_field_one.getText().toString());
+                float num2 = Float.parseFloat(number_field_two.getText().toString());
+                float result = num1 + num2;
+                resultTextView.setText(String.valueOf(result));
+            }
+        });
+
+
+
     }
 }
+
+
+
