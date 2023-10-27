97f5113a12de115d31e2f967f18e8d98da63076c
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-10-27 16:58:00 +0300

    DisignActivity

---
+++ app/src/main/java/ua/com/calculation/DesignActivity.java
@@ -0,0 +1,14 @@
+package ua.com.calculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disign);
+    }
+}
