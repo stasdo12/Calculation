4d5a2a9f49f589307505f2a1d4d9391cb9fbf898
Stanislav Donetc <123019654+DonStMi@users.noreply.github.com>
2023-11-02 14:44:22 +0200

    Action handlers. Gesture tracking

--- app/src/main/java/ua/com/calculation/ListViewActivity.java
+++ app/src/main/java/ua/com/calculation/ListViewActivity.java
@@ -1,19 +1,26 @@
 package ua.com.calculation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
     private String[] namesArr = new String[]{"Jon", "Stan", "Bob", "Georg", "Alex"};
     private ListView listView;
+
+    private GestureDetectorCompat gestureDetectorCompat;
 
 
 
@@ -23,6 +30,9 @@ public class ListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         listView = findViewById(R.id.ListView);
+        gestureDetectorCompat = new GestureDetectorCompat(this, this);
+        gestureDetectorCompat.setIsLongpressEnabled(true);
+        gestureDetectorCompat.setOnDoubleTapListener(this);
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -30,12 +40,72 @@ public class ListViewActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
 
 
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewActivity.this,
+                        (String)listView.getItemAtPosition(position), Toast.LENGTH_LONG ).show();
+            }
+        });
+
+
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
     public void moveToMainPage(View view){
         Intent intent = new Intent(this, TextEditorActivity.class);
         startActivity(intent);
     }
 
+    @Override
+    public boolean onDown(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(@NonNull MotionEvent e) {
+        Toast.makeText(this,  e.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(@NonNull MotionEvent e) {
+        return false;
+    }
 }
